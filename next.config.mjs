d0a58e182e633a07fc4eb6356c8e47bc200b45c6
Willy Arisky <willy.arisky@gmail.com>
2025-09-28 12:59:28 +0700

    feat: add URL rewrite rule to serve latest.zip from GitHub main branch

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,14 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/get/latest.zip',
+        destination: 'https://github.com/0php/Zero/archive/refs/heads/main.zip',
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
